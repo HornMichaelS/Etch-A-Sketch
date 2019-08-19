f7d2b9e7fc38a29d28829a31b076de025d040ff5
Michael Horn <hornmichaels@gmail.com>
2019-08-19 11:41:36 -0700

    Finished redesign/refactor

--- js/sketch.js
+++ js/sketch.js
@@ -1,57 +1,123 @@
 const SIDE_LENGTH = 640;
 
-function createDomGrid(n) {
-  const grid = document.createElement('div');
-  grid.setAttribute('id', 'grid');
+const Modes = Object.freeze({
+  normal: "normal",
+  colors: "colors",
+  greyscale: "greyscale",
+});
 
-  for (let i = 0; i < n; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < n; j++) {
-      const cell = document.createElement('div');
-      const cellSideLength = `${(SIDE_LENGTH / n).toFixed(1)}px`;
+function EtchASketch() {
+  const SIDE_LENGTH = 640;
 
-      cell.classList.add('grid-cell');
-      cell.style.width = cellSideLength;
-      cell.style.height = cellSideLength;
+  let cellsPerRow = 16;
+  let mountedAt;
+  let grid;
+  let mode = Modes.normal;
 
-      let opacity = 0;
-
-      cell.addEventListener('mouseenter', (e) => {
-        opacity += 0.1;
-
-        cell.classList.add('hovered');
-        cell.style.opacity = opacity;
-        cell.style.backgroundColor = 'black';
-      })
-
-      row.append(cell);
+  function getNormalModeListener(cell) {
+    return function () {
+      cell.style.backgroundColor = "black";
     }
-    grid.append(row);
   }
 
-  return grid;
+  function getColorsModeListener(cell) {
+    function randomRGBVal() {
+      return Math.floor(Math.random() * 256);
+    }
+    return function () {
+      const red = randomRGBVal();
+      const green = randomRGBVal();
+      const blue = randomRGBVal();
+
+      cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
+  }
+
+  function getGreyscaleModeListener(cell) {
+    let opacity = 0;
+    return function () {
+      opacity += 0.1;
+      cell.style.backgroundColor = "black";
+      cell.style.opacity = opacity;
+    }
+  }
+
+  function createDomGrid() {
+    grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+
+    for (let i = 0; i < cellsPerRow; i++) {
+      const row = document.createElement('div');
+      row.classList.add('row');
+      for (let j = 0; j < cellsPerRow; j++) {
+        const cell = document.createElement('div');
+        const cellSideLength = `${(SIDE_LENGTH / cellsPerRow).toFixed(1)}px`;
+
+        cell.classList.add('grid-cell');
+        cell.style.width = cellSideLength;
+        cell.style.height = cellSideLength;
+
+        let listener = getNormalModeListener(cell);
+
+        switch (mode) {
+          case Modes.normal:
+            listener = getNormalModeListener(cell);
+            break;
+          case Modes.colors:
+            listener = getColorsModeListener(cell);
+            break;
+          case Modes.greyscale:
+            listener = getGreyscaleModeListener(cell);
+            break;
+        }
+
+        cell.addEventListener('mouseenter', listener);
+
+        row.append(cell);
+      }
+      grid.append(row);
+    }
+  }
+
+  const reset = (function () {
+    grid.remove();
+    createDomGrid();
+    this.mount(mountedAt);
+  }).bind(this);
+
+  this.changeMode = function (newMode) {
+    mode = newMode;
+    reset();
+  }
+
+  this.mount = function (selector) {
+    mountedAt = selector;
+    document.querySelector(selector).append(grid);
+  }
+
+  this.shake = function () {
+    cellsPerRow = prompt('How many cells per side?');
+    reset();
+  }
+
+  createDomGrid();
 }
 
-function insertGrid(grid) {
-  document.querySelector('.container>.top-buttons-container').after(grid);
-}
-
-function clearGrid() {
-  const grid = document.querySelector('#grid');
-  grid.remove();
-}
-
-function shakeGrid() {
-  const n = prompt('What should the new grid size be?');
-  const newGrid = createDomGrid(n);
-
-  clearGrid();
-  insertGrid(newGrid);
-}
-
-insertGrid(createDomGrid(16));
+let etchASketch = new EtchASketch();
+etchASketch.mount('.grid-container');
 
 document.querySelector('#shakeButton').addEventListener('click', () => {
-  shakeGrid();
+  etchASketch.shake()
+});
+
+document.querySelector('#normalButton').addEventListener('click', () => {
+  etchASketch.changeMode(Modes.normal);
+})
+
+document.querySelector('#colorsButton').addEventListener('click', () => {
+  etchASketch.changeMode(Modes.colors);
+})
+
+document.querySelector('#greyscaleButton').addEventListener('click', () => {
+  etchASketch.changeMode(Modes.greyscale);
 })
